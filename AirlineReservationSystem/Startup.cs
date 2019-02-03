218e69c36fdab2ef511eb7c8f71a70a59a949a43
RutviShah2008 <200390373@student.georgianc.on.ca>
2019-02-03 01:57:20 -0500

    Add project files.

---
+++ AirlineReservationSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirlineReservationSystem.Startup))]
+namespace AirlineReservationSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
