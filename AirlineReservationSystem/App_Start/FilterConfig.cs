216db8c66adf3e83cbcdc2b1bb788464992f99a5
RutviShah2008 <200390373@student.georgianc.on.ca>
2019-03-19 02:32:21 -0400

    Added  Authorization

--- AirlineReservationSystem/App_Start/FilterConfig.cs
+++ AirlineReservationSystem/App_Start/FilterConfig.cs
@@ -8,6 +8,8 @@ namespace AirlineReservationSystem
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
